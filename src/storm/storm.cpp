f7fa1145cc102348ce1131e5ac799d8d5c2864f3
Daniel Basgöze <74880317+dbasgoeze@users.noreply.github.com>
2021-04-12 13:00:30 +0200

    Test

--- src/storm/storm.cpp
+++ src/storm/storm.cpp
@@ -17,13 +17,6 @@
  */
 int main(const int argc, const char** argv) {
 
-    try {
-        return storm::cli::process(argc, argv);
-    } catch (storm::exceptions::BaseException const& exception) {
-        STORM_LOG_ERROR("An exception caused Storm to terminate. The message of the exception is: " << exception.what());
-        return 1;
-    } catch (std::exception const& exception) {
-        STORM_LOG_ERROR("An unexpected exception occurred and caused Storm to terminate. The message of this exception is: " << exception.what());
-        return 2;
-    }
+    this should not work and is a test to see if the CI spots this and so on.
+    Why are you reading this?!
 }
